8d683f9b23e7d3daacc8a787172c1c5b319becab
LeonardoEichs <leonardoeichs@gmail.com>
2017-05-23 23:47:19 -0300

    Comments hpp

--- Environment.hpp
+++ Environment.hpp
@@ -7,25 +7,45 @@ using namespace std;
 
 class Environment {
   public:
+    // Vector containing population
     vector<Individual> population;
+    // Number of members in the population
     int population_size;
-    int max_generations;
-    double best_score;
-    int best_repetition;
-    int max_repetitions;
+    // Keeps track of which generation the algorithm is in
     int generation;
+    // Sets a maximum generation value in which the program stops
+    int max_generations;
+    // Sets a maximum number of generation in which the best score repeats,
+    // in case of the best score repeating we can stop the program earlier
+    int max_repetitions;
+    // Keeps track of the number of generations in which the best score is repeated
+    int best_repetition;
+    // Keeps track of the best score in the population
+    double best_score;
+    // Rate in which the operation of crossover will occur
     double crossover_rate;
+    // Rate in which the operation of mutation will occur
     double mutation_rate;
   public:
-    Environment(int pop_size, int max_gen, double cross_rate, double mut_rate);
+    // Environment constructor defines the class variables
+    // [population_size, max_generations, max_repetitions, crossover_rate, mutation_rate]
+    Environment(int pop_size, int max_gen, int max_rep, double cross_rate, double mut_rate);
+    // Prints the populations cromossomes and fitness scores
     void displayPopulation();
-    void run();
-    void step();
-    int tournament();
-    Individual _mutate(Individual ind);
-    double average();
-    Individual best();
+    // Test if the best score changed from the previous generation to the actual
     bool test_variation();
+    // Runs the genetic algorithm
+    void run();
+    // Executes a step of the algorithm
+    void step();
+    // Selection of cromossome to go the next generation
+    int tournament();
+    // Mutates the individual
+    Individual _mutate(Individual ind);
+    // Average score of the population
+    double average();
+    // Returns best individual of the population
+    Individual best();
 };
 
 #endif
--- Individual.hpp
+++ Individual.hpp
@@ -6,14 +6,22 @@ using namespace std;
 
 class Individual {
   public:
+    // Score of the individual
     double fitness_score;
+    // Array of bits that represent an individual
     vector<bool> cromossome;
   public:
+    // Creates a random cromossome and evaluates
     Individual();
-    void evaluate();
+    // Normalize the value of the cromossome to be used in the evaluate function
     double normalize();
+    // Function responsible for assigning a score to the individual
+    void evaluate();
+    // Mutation operation, invert the value of a bit
     void mutate(int position);
+    // Crossover operation
     vector<vector<bool> > crossover(vector<bool> cromossome2);
+    // Display the cromossome
     void getCromossomeRepr();
 };
 
